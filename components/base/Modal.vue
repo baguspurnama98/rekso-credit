4ed5ea14443ce5a696b70085e668b5e8e1437f93
baguspurnama98 <baguspurnama98@gmail.com>
2022-10-20 06:05:08 +0700

    Add livin qr code

---
+++ components/base/Modal.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="modal-overlay" @click="$emit('close-modal')">
+    <div class="modal text-center px-10" @click.stop>
+      <img class="check mx-auto" :src="require('~/assets/img/qrcode-livin.jpeg')" alt="" />
+      <h3 class="text-header-gradient font-extrabold text-2xl">Livin' By Mandiri</h3>
+      <p class="px-5 py-0">
+        Nikmati gampangnya buka rekening online, transaksi, dan integrasi segala kebutuhan finansial kamu hanya dengan
+        <b>Livin' by Mandiri</b>
+      </p>
+      <button @click="$emit('close-modal')">Close</button>
+    </div>
+    <!-- <div class="close" @click="$emit('close-modal')"> -->
+      <!-- <img class="close-img" src="~/assets/close-icon.svg" alt="" /> -->
+      <!-- <img class="close-img" :src="require('~/assets/img/qr-code.png')" alt="" /> -->
+      <!-- X
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ModalLivin',
+}
+</script>
+<style scoped>
+.text-header-gradient {
+  background: rgb(57, 132, 244);
+  background: linear-gradient(169.4deg, #3984f4 -6.01%, #0cd3ff 36.87%, #2f7cf0 78.04%, #0e65e8 103.77%);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  background-color: #000000da;
+}
+
+.modal {
+  text-align: center;
+  background-color: white;
+  height: 500px;
+  width: 500px;
+  margin-top: 10%;
+  padding: 60px 0;
+  border-radius: 20px;
+}
+.close {
+  margin: 10% 0 0 16px;
+  cursor: pointer;
+}
+
+.close-img {
+  width: 25px;
+}
+
+.check {
+  width: 200px;
+}
+
+h6 {
+  font-weight: 500;
+  font-size: 28px;
+  margin: 20px 0;
+}
+
+p {
+  font-size: 16px;
+  margin: 20px 0;
+}
+
+button {
+  background-color: #ac003e;
+  width: 150px;
+  height: 40px;
+  color: white;
+  font-size: 14px;
+  border-radius: 16px;
+  margin-top: 50px;
+}
+</style>
--- components/base/Navbar.vue
+++ components/base/Navbar.vue
@@ -1,83 +1,60 @@
 <template>
-  <nav id="navbar" class="relative z-10 w-full text-neutral-800 ">
+  <nav id="navbar" class="relative z-10 w-full text-neutral-800">
     <div class="flex flex-col max-w-screen-xl px-8 mx-auto lg:items-center lg:justify-between lg:flex-row py-4">
-      <div class="flex flex-col lg:flex-row items-center space-x-4 xl:space-x-8">
-        <div class="w-full flex flex-row items-center justify-between py-6">
-          <div>
-            <a href="/">
-            <img :src="require('~/assets/img/logo/Bank_Mandiri_logo_2016.png')" class="w-32 xl:w-44" alt="Mandiri Logo" />
+      <div class="w-full flex flex-row items-center justify-between py-6">
+        <div>
+          <a href="/">
+            <img
+              :src="require('~/assets/img/logo/Bank_Mandiri_logo_2016.png')"
+              class="w-32 xl:w-44"
+              alt="Mandiri Logo"
+            />
           </a>
-          </div>
-          <!-- <button class="rounded-lg lg:hidden focus:outline-none focus:shadow-outline" @click="open = !open">
-            <SegmentIcon v-if="!open" :size="24" />
-            <CloseIcon v-else :size="24" />
-          </button> -->
         </div>
-        <!-- <ul
-          :class="[open ? 'flex' : 'hidden lg:flex']"
-          class="w-full h-auto flex flex-col flex-grow lg:items-center pb-4 lg:pb-0 lg:justify-end lg:flex-row origin-top duration-300 xl:space-x-2 space-y-3 lg:space-y-0"
-        >
-          <NavLink name="Cryptocurrency" url="#" />
-          <NavLink name="Exchanges" url="#" />
-          <NavLink name="Watchlist" url="#" />
-          <NavLink name="NFT" url="#" />
-          <NavLink name="Portfolio" url="#" />
-          <li class="relative group">
-            <button
-              class="md:px-4 py-2 text-sm bg-transparent rounded-lg text-[#666666] hover:text-gray-900 focus:outline-none focus:shadow-outline flex items-center"
-              @click="dropdownToggler"
-              @blur="dropdownToggler"
-            >
-              <span>Products</span>
-              <ChevronUpIcon v-if="dropdownNavbar" :size="16" />
-              <ChevronDownIcon v-else :size="16" />
-            </button>
-            <transition name="transform-fade-down">
-              <ul
-                v-if="dropdownNavbar"
-                class="flex lg:absolute flex-col max-w-42 py-1 lg:bg-white rounded-md lg:shadow-md pl-2 lg:pl-0"
-              >
-                <li>
-                  <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">Exchange</a>
-                </li>
-                <li>
-                  <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">Wallet</a>
-                </li>
-                <li>
-                  <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">Explorer</a>
-                </li>
-                <li>
-                  <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100">Charts</a>
-                </li>
-              </ul>
-            </transition>
-          </li>
-        </ul> -->
+        <div>
+          <base-button
+            class="px-5 py-3 bg-inherit text-gradient border border-[#0c66ee] flex sm:text-base text-sm"
+            @click="showModal = true"
+          >
+            <img class="fill-current w-6 h-6" :src="require('~/assets/img/qr-code.png')" alt="" />
+            <span class="hidden ml-2 sm:flex">Livin' by Mandiri</span>
+          </base-button>
+        </div>
       </div>
-      <!-- <div :class="[open ? 'flex' : 'hidden lg:flex']" class="space-x-3">
-        <base-button class="px-8 xl:px-10 py-3 mt-2 bg-inherit text-gradient border border-[#0c66ee]">
-          Login
-        </base-button>
-        <base-button class="px-8 xl:px-10 py-3 mt-2 bg-gradient-to-r from-[#468ef9] to-[#0c66ee] text-white">
-          Sign Up
-        </base-button>
-      </div> -->
     </div>
+    <!-- <modal v-if="isShowQR" title="This is a test!" @close="showQR = false" class="animated fadeIn">
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </p>
+      </div>
+    </modal> -->
+    <SavedModal v-show="showModal" @close-modal="showModal = false"/>
   </nav>
 </template>
 <script>
+import SavedModal from './Modal'
 export default {
   name: 'BaseNavbar',
+  components: { SavedModal },
   data() {
     return {
       open: false,
       dropdownNavbar: false,
+      showModal: false,
     }
   },
   methods: {
-    dropdownToggler () {
+    dropdownToggler() {
       this.dropdownNavbar = !this.dropdownNavbar
-    }
-  }
+    },
+    showQR() {
+      this.showQR = !this.showQR
+    },
+  },
 }
 </script>
